bbda63b53065003a181729e636818729820e52d9
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-03-10 15:41:26 -0800

    [refactor] stubbed createPatient function iin newPatientCtrl.js

--- client/src/components/newPatient/newPatientCtrl.js
+++ client/src/components/newPatient/newPatientCtrl.js
@@ -1,4 +1,9 @@
 angular.module('app.newPatient', [])
   .controller('newPatientCtrl', [ function(){
+    var that = this;
+    that.data = {};
 
+    that.createPatient = function(){
+      //TODO send to Factory- POST
+    };
   }]);
