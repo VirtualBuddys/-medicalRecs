9b4e44321eb667f858a72c2a340f5b4a9ef054b5
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-03-14 20:01:04 -0700

    [refactor] added that.data keys in newPatientCtrl

--- client/src/components/newPatient/newPatientCtrl.js
+++ client/src/components/newPatient/newPatientCtrl.js
@@ -1,9 +1,46 @@
 angular.module('app.newPatient', [])
-  .controller('newPatientCtrl', [ function(){
+  .controller('newPatientCtrl', ['newPatientModel', function(newPatientModel){
     var that = this;
     that.data = {};
+    that.data.prefix;
+    that.data.firstName;
+    that.data.middleInitial;
+    that.data.lastName;
+    that.data.sex;
+    that.data.ssn;
+    that.data.address1;
+    that.data.city;
+    that.data.stateUsa;
+    that.data.zipCode;
+    that.data.homeNumber;
+    that.data.cellNumber;
+    that.data.workNumber;
+    that.data.primaryNumber;
+    that.data.email;
+    that.data.prefixKin;
+    that.data.firstNameKin;
+    that.data.middleInitialKin;
+    that.data.lastNameKin;
+    that.data.relation;
+    that.data.numberKin;
+
 
     that.createPatient = function(){
-      //TODO send to Factory- POST
+      //TODO
+        //check to see if all the necessary fields are filled
+        //put it into the patient object
+      console.log('CREATE PATIENT HAS BEEN CALLED');
+      var patient = that.data;
+
+      newPatientModel.postNewPatient(patient)
+          .then(function(results){
+            console.log('SUCCESS INSIDE CREATEPATIENT CTRL');
+          })
+          .catch(function(error){
+            console.log(error, 'ERROR INSIDE CREATE PATIENT CTRL');
+          })
+
+
+
     };
   }]);
