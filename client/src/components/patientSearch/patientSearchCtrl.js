eacba36e807b0c66e6769e17d22472bceb24fe71
Elizabeth Banalagay <zbanalagay@gmail.com>
2016-03-15 18:40:32 -0700

    [strcture] stubbed out functions in patientSearchCtrl.js

--- client/src/components/patientSearch/patientSearchCtrl.js
+++ client/src/components/patientSearch/patientSearchCtrl.js
@@ -1,4 +1,22 @@
 angular.module('app.patientSearch', [])
-  .controller('patientSearchCtrl', [ function(){
+  .controller('patientSearchCtrl', ['$state', function($state){
+    var that = this;
+    that.data = {};
 
-  }]);
+    //TODO put an autocomplete that syncs up with the database;
+
+    that.getAllPatientsThatFit = function(searchedQuery){
+
+    };
+
+    var changeState = function(patientId){
+     //TODO fill in the $state.go
+    };
+
+    that.goToPatientProfile = function(patient){
+      //TODO get the necessary stuff to pass in
+      //call changeState();
+    };
+
+
+ }]);
